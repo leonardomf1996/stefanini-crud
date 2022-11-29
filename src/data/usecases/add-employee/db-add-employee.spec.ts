c7cf8d901d33034f3b13edbfb755f215ce97259c
Leonardo Furtado <leonardomf1996@gmail.com>
2022-11-29 20:51:32 -0300

    test: ensure DbAddEmployee call AddEmployeeRepository with correct values

---
+++ src/data/usecases/add-employee/db-add-employee.spec.ts
@@ -0,0 +1,53 @@
+import { AddEmployee, AddEmployeeModel, AddEmployeeRepository, EmployeeModel } from "./db-add-employee-protocols";
+import { DbAddEmployee } from "./db-add-employee";
+
+interface SutTypes {
+   sut: DbAddEmployee;
+   addEmployeeRepositoryStub: AddEmployeeRepository;
+}
+
+const makeAddEmployeeRepository = (): AddEmployeeRepository => {
+   class AddEmployeeRepositoryStub implements AddEmployeeRepository {
+      async add(employeeData: AddEmployeeModel): Promise<EmployeeModel> {
+         const fakeEmployee = {
+            id: 'valid_id',
+            name: 'valid_name',
+            age: 'valid_age',
+            role: 'valid_role',
+         }
+
+         return new Promise(resolve => resolve(fakeEmployee));
+      }
+   }
+
+   return new AddEmployeeRepositoryStub();
+}
+
+const makeSut = (): SutTypes => {
+   const addEmployeeRepositoryStub = makeAddEmployeeRepository();
+   const sut = new DbAddEmployee(addEmployeeRepositoryStub);
+   return { sut, addEmployeeRepositoryStub };
+}
+
+describe('DbAddAccount use case', () => {
+   test('should call AddAccountRepository with correct values', async () => {
+      const { sut, addEmployeeRepositoryStub } = makeSut();
+
+      const saveSpy = jest.spyOn(addEmployeeRepositoryStub, 'add');
+
+      const employeeData = {
+         name: 'valid_name',
+         age: 'valid_age',
+         role: 'valid_role',
+      }
+
+      await sut.add(employeeData);
+
+      await expect(saveSpy).toHaveBeenCalledWith({
+         name: 'valid_name',
+         age: 'valid_age',
+         role: 'valid_role',
+      })
+   });
+
+});
