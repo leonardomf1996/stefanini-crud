0d14687e0f1e94ffa2720ec861ecf4b2392724d2
Leonardo Furtado <leonardomf1996@gmail.com>
2022-11-30 20:28:09 -0300

    test: ensure DeleteEmployeeController calls DeleteEmployee with correct value

--- src/presentation/controller/employee/delete-employee.spec.ts
+++ src/presentation/controller/employee/delete-employee.spec.ts
@@ -1,13 +1,25 @@
+import { DeleteEmployee, DeleteEmployeeModel } from "../../../domain/usecases/delete-employee";
 import { MissingParamError } from "../../errors";
 import { DeleteEmployeeController } from "./delete-employee";
 
 interface SutType {
    sut: DeleteEmployeeController;
+   deleteEmployeeStub: DeleteEmployee;
+}
+
+const makeDeleteEmployee = (): DeleteEmployee => {
+   class DeleteEmployeeStub implements DeleteEmployee {
+      async delete(employee: DeleteEmployeeModel): Promise<void> {
+         return new Promise(resolve => resolve(null));
+      }
+   }
+   return new DeleteEmployeeStub();
 }
 
 const makeSut = (): SutType => {
-   const sut = new DeleteEmployeeController();
-   return { sut };
+   const deleteEmployeeStub = makeDeleteEmployee();
+   const sut = new DeleteEmployeeController(deleteEmployeeStub);
+   return { sut, deleteEmployeeStub };
 }
 
 describe('DeketeEmployee Controller', () => {
@@ -22,5 +34,23 @@ describe('DeketeEmployee Controller', () => {
 
       expect(httpResponse.statusCode).toBe(400);
       expect(httpResponse.body).toEqual(new MissingParamError('id'));
+   })
+
+   test('Should call DeleteEmployee with correct values', async () => {
+      const { sut, deleteEmployeeStub } = makeSut();
+
+      const deleteSpy = jest.spyOn(deleteEmployeeStub, 'delete');
+
+      const httpRequest = {
+         params: {
+            id: 'valid_id'
+         }
+      }
+
+      await sut.handle(httpRequest);
+
+      expect(deleteSpy).toHaveBeenCalledWith({
+         id: 'valid_id'
+      });
    })
 })
